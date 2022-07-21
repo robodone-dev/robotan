f7534cae4d81b615c19ad16db1438ff4224bbd87
robodone-dev <71869290+robodone-dev@users.noreply.github.com>
2022-07-21 12:26:11 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+
 particlesJS("particles-js", {"particles":{"number":{"value":80,"density":{"enable":true,"value_area":800}},"color":{"value":"#2fff14"},"shape":{"type":"circle","stroke":{"width":0,"color":"#000000"},"polygon":{"nb_sides":5},"image":{"src":"img/github.svg","width":100,"height":100}},"opacity":{"value":0.809723090737089,"random":false,"anim":{"enable":false,"speed":1,"opacity_min":0.1,"sync":false}},"size":{"value":3,"random":true,"anim":{"enable":false,"speed":40,"size_min":0.1,"sync":false}},"line_linked":{"enable":true,"distance":208.44356791251798,"color":"#37b4e8","opacity":0.6894671861721748,"width":1},"move":{"enable":true,"speed":6,"direction":"none","random":false,"straight":false,"out_mode":"out","bounce":false,"attract":{"enable":false,"rotateX":600,"rotateY":1200}}},"interactivity":{"detect_on":"canvas","events":{"onhover":{"enable":true,"mode":"repulse"},"onclick":{"enable":true,"mode":"push"},"resize":true},"modes":{"grab":{"distance":400,"line_linked":{"opacity":1}},"bubble":{"distance":400,"size":40,"duration":2,"opacity":8,"speed":3},"repulse":{"distance":200,"duration":0.4},"push":{"particles_nb":4},"remove":{"particles_nb":2}}},"retina_detect":true});
 
 
@@ -35,7 +36,7 @@ window.addEventListener("load", ()=>{
     function addCount(){
         n++;
         counter.innerHTML = n;
-        var byn = n * 10;
+        var byn = n;
         progress_move(byn);
     }
 
@@ -49,7 +50,14 @@ window.addEventListener("load", ()=>{
     });
   
     btn.addEventListener('click', function() {
-      next();
+      //alert(counter.innerHTML);
+      if(counter.innerHTML > 4){
+        openModal();
+      } else {
+        next();
+      }
+      
+      
     });
     
     function next() {
@@ -57,6 +65,7 @@ window.addEventListener("load", ()=>{
         //card.addEventListener('transitionend', readworddata);
         flip();
       } else {
+        addCount();
         readworddata();
       }
     }
@@ -98,7 +107,7 @@ window.addEventListener("load", ()=>{
     btnAdd = document.getElementById("btn");
     n = 0;
  
-    btnAdd.addEventListener("click", addCount);
+    //btnAdd.addEventListener("click", addCount);
     }); 
 
     function flip() {
@@ -115,6 +124,10 @@ window.addEventListener("load", ()=>{
         function frame() {
           if (width >= 110) {
             //clearInterval(id);
+            var count_value = 0;
+            width = 0;
+            elem.style.width = width + "%";
+            elem.innerHTML = width + "%";
             i = 0;
           } else {
             //width++;
@@ -125,3 +138,31 @@ window.addEventListener("load", ()=>{
     }
 
   })();
+
+const modal = document.querySelector(".modal"); //modalを指定
+const overlay = document.querySelector(".overlay"); //overlayを指定
+const btnOpenModal = document.querySelector(".show-modal"); //modalを開くボタンを指定
+const btnCloseModal = document.querySelector(".close-modal"); //modalを閉じるボタンを指定
+
+//modalとoverlayのhiddenクラスを消す（modalとoverlayが見えるようにする）処理
+const openModal = () => {
+  modal.classList.remove("hidden");
+  overlay.classList.remove("hidden");
+};
+
+//modalとoverlayのhiddenクラスを追加する（modalとoverlayが見えないようにする）処理
+const closeModal = () => {
+  modal.classList.add("hidden");
+  overlay.classList.add("hidden");
+  doReload();
+};
+
+//modalの開くボタンと閉じるボタンをクリックした時の処理
+btnOpenModal.addEventListener("click", openModal);
+btnCloseModal.addEventListener("click", closeModal);
+
+function doReload() {
+ 
+    // reloadメソッドによりページをリロード
+    window.location.reload();
+}
