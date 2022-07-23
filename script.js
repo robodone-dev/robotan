95c1e67005b656337b8dda711ae300e00af970a4
robodone-dev <71869290+robodone-dev@users.noreply.github.com>
2022-07-23 14:52:49 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-particlesJS("particles-js", {"particles":{"number":{"value":80,"density":{"enable":true,"value_area":800}},"color":{"value":"#2fff14"},"shape":{"type":"circle","stroke":{"width":0,"color":"#000000"},"polygon":{"nb_sides":5},"image":{"src":"img/github.svg","width":100,"height":100}},"opacity":{"value":0.809723090737089,"random":false,"anim":{"enable":false,"speed":1,"opacity_min":0.1,"sync":false}},"size":{"value":3,"random":true,"anim":{"enable":false,"speed":40,"size_min":0.1,"sync":false}},"line_linked":{"enable":true,"distance":208.44356791251798,"color":"#37b4e8","opacity":0.6894671861721748,"width":1},"move":{"enable":true,"speed":6,"direction":"none","random":false,"straight":false,"out_mode":"out","bounce":false,"attract":{"enable":false,"rotateX":600,"rotateY":1200}}},"interactivity":{"detect_on":"canvas","events":{"onhover":{"enable":true,"mode":"repulse"},"onclick":{"enable":true,"mode":"push"},"resize":true},"modes":{"grab":{"distance":400,"line_linked":{"opacity":1}},"bubble":{"distance":400,"size":40,"duration":2,"opacity":8,"speed":3},"repulse":{"distance":200,"duration":0.4},"push":{"particles_nb":4},"remove":{"particles_nb":2}}},"retina_detect":true});
+particlesJS("particles-js", {"particles":{"number":{"value":43,"density":{"enable":true,"value_area":561.194221302933}},"color":{"value":"#ffffff"},"shape":{"type":"star","stroke":{"width":0,"color":"#000000"},"polygon":{"nb_sides":5},"image":{"src":"http://wiki.lexisnexis.com/academic/images/f/fb/Itunes_podcast_icon_300.jpg","width":100,"height":100}},"opacity":{"value":0.7891476416322727,"random":true,"anim":{"enable":false,"speed":1,"opacity_min":0.1,"sync":false}},"size":{"value":6,"random":true,"anim":{"enable":false,"speed":7.308694910712106,"size_min":0.1,"sync":false}},"line_linked":{"enable":false,"distance":150,"color":"#ffffff","opacity":0.4,"width":1},"move":{"enable":true,"speed":1.5,"direction":"bottom-left","random":false,"straight":true,"out_mode":"out","bounce":false,"attract":{"enable":false,"rotateX":0,"rotateY":0}}},"interactivity":{"detect_on":"canvas","events":{"onhover":{"enable":false,"mode":"grab"},"onclick":{"enable":false,"mode":"repulse"},"resize":true},"modes":{"grab":{"distance":0,"line_linked":{"opacity":1}},"bubble":{"distance":599.4005994005994,"size":23.976023976023978,"duration":2,"opacity":8,"speed":3},"repulse":{"distance":200,"duration":0.4},"push":{"particles_nb":4},"remove":{"particles_nb":2}}},"retina_detect":true});
 
 
 var counter, btnAdd,optionMode;
@@ -53,7 +53,12 @@ window.addEventListener("load", ()=>{
 
     select1.addEventListener('input',function(){
       //alert("select1");
+      if (card.className === 'open') {
+        flip();
+      }
       url = 'https://api.sssapi.app/ikn2OFJAdigxOScXHcTZp';
+      optionAudio();
+      backgroundColor1();
       readworddata(url);
       progress_reset();
       countReset();
@@ -61,7 +66,15 @@ window.addEventListener("load", ()=>{
     });
     select2.addEventListener('input',function(){
       //alert("select2");
+      if (card.className === 'open') {
+        flip();
+      }
       url = 'https://api.sssapi.app/tVPUcQK6fxwwSnGoliqDJ';
+      if (card.className === 'open') {
+        flip();
+      }
+      optionAudio();
+      backgroundColor2();
       readworddata(url);
       progress_reset();
       countReset();
@@ -69,15 +82,25 @@ window.addEventListener("load", ()=>{
     });
     select3.addEventListener('input',function(){
       //alert("select3");
+      if (card.className === 'open') {
+        flip();
+      }
       url = 'https://api.sssapi.app/8-hJmLsQlPA1ag-99JzFJ';
+      optionAudio();
       readworddata(url);
+      backgroundColor3();
       progress_reset();
       countReset();
       randoms.length = 0;
     });
     select4.addEventListener('input',function(){
       //alert("select4");
+      if (card.className === 'open') {
+        flip();
+      }
       url = 'https://api.sssapi.app/ikn2OFJAdigxOScXHcTZp';
+      optionAudio();
+      backgroundColor4();
       readworddata(url);
       progress_reset();
       countReset();
@@ -112,6 +135,7 @@ window.addEventListener("load", ()=>{
       } else {
         addCount();
         readworddata(url);
+        nextAudio();
       }
     }
 
@@ -194,6 +218,7 @@ window.addEventListener("load", ()=>{
 
     function flip() {
       card.className = card.className === '' ? 'open' : '';
+      cardAudio();
     }
   
     function countReset(){
@@ -261,4 +286,47 @@ btnCloseModal.addEventListener("click", closeModal);
 function doReload() {
     // reloadメソッドによりページをリロード
     window.location.reload();
+}
+
+function backgroundColor1() {
+    var element = document.querySelector('#particles-js');
+    element.style.background='url("https://robodone-dev.github.io/robotan/220723_2.gif"),linear-gradient(31deg, rgba(196, 182, 197, 1) 10%, rgba(35, 117, 163, 1))'
+    element.style.backgroundRepeat ="no-repeat,repeat";
+    element.style.backgroundPosition ="50% 6%";
+}
+
+function backgroundColor2() {
+    var element = document.querySelector('#particles-js');
+    element.style.background='url("https://robodone-dev.github.io/robotan/220723_2.gif"),linear-gradient(31deg, rgba(35, 117, 163, 1) 10%, rgba(142, 84, 219, 1))';
+    element.style.backgroundRepeat ="no-repeat,repeat";
+    element.style.backgroundPosition ="50% 6%";
+}
+
+function backgroundColor3() {
+    var element = document.querySelector('#particles-js');
+    element.style.background='url("https://robodone-dev.github.io/robotan/220723_2.gif"),linear-gradient(31deg, rgba(142, 84, 219, 1) 10%, rgba(237, 178, 92, 1))';
+    element.style.backgroundRepeat ="no-repeat,repeat";
+    element.style.backgroundPosition ="50% 6%";
+}
+
+function backgroundColor4() {
+    var element = document.querySelector('#particles-js');
+    element.style.background='url("https://robodone-dev.github.io/robotan/220723_2.gif"),linear-gradient(31deg, rgba(237, 178, 92, 1) 10%, rgba(231, 158, 223, 1))';
+    element.style.backgroundRepeat ="no-repeat,repeat";
+    element.style.backgroundPosition ="50% 6%";
+}
+
+function cardAudio() {
+    document.getElementById('card_audio').currentTime = 0; //連続クリックに対応
+    document.getElementById('card_audio').play(); //クリックしたら音を再生
+}
+
+function optionAudio() {
+    document.getElementById('option_audio').currentTime = 0; //連続クリックに対応
+    document.getElementById('option_audio').play(); //クリックしたら音を再生
+}
+
+function nextAudio() {
+    document.getElementById('next_audio').currentTime = 0; //連続クリックに対応
+    document.getElementById('next_audio').play(); //クリックしたら音を再生
 }
